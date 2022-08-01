402b11acef8bd715e1bca6d74a8c348c0ecdd504
shenguanjiejie <835166018@qq.com>
2022-08-01 13:13:09 +0800

    2022-08-01 13:12:39 ISO 8601 to UTC time, add build.sh

--- cmd/readme.go
+++ cmd/readme.go
@@ -31,7 +31,7 @@ var readmeCmd = &cobra.Command{
 	Long:  `readme`,
 	Run: func(cmd *cobra.Command, args []string) {
 		worksAnchor, works := works()
-		version := "0.0.1"
+		version := "0.0.2"
 		download := "or<br><br>[Download](https://github.com/shenguanjiejie/workwork/releases), Unpack, and move \"ww\" to /usr/local/bin.<br>到[release页面](https://github.com/shenguanjiejie/workwork/releases)下载后, 解压出ww文件, 放在/usr/local/bin目录下即可\n"
 		macInstall := fmt.Sprintf("# Install\n### Mac\n```shell\nbrew install shenguanjiejie/tap/workwork\n```\nor\n```shell\ncurl -LO https://github.com/shenguanjiejie/workwork/releases/download/v%s/workwork_%s_darwin.tar.gz && tar -zxvf ./workwork_%s_darwin.tar.gz && mv ./ww /usr/local/bin && rm ./workwork_%s_darwin.tar.gz\n```\n%s", version, version, version, version, download)
 		linuxInstall := fmt.Sprintf("### Linux\n```shell\ncurl -LO https://github.com/shenguanjiejie/workwork/releases/download/v%s/workwork_%s_linux_x86_64.tar.gz && tar -zxvf ./workwork_%s_linux_x86_64.tar.gz && mv ./ww /usr/local/bin && rm ./workwork_%s_linux_x86_64.tar.gz\n```\n%s", version, version, version, version, download)
